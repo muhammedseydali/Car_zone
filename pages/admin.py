07d3f42e549121370d2d994d50ec546184a97ad5
muhammed seydali <60098100+muhammedseydali@users.noreply.github.com>
2022-05-13 13:52:19 +0530

    completed admin panel modification

--- pages/admin.py
+++ pages/admin.py
@@ -1,5 +1,25 @@
-from re import T
+from django.utils.html import format_html
 from django.contrib import admin
 from pages.models import Team
+
 # Register your models here.
-admin.site.register(Team)
+
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self,object):
+        return format_html('<img src="{}" width="40" style="border-radius:50px;" />'.format(object.photo.url))
+
+    thumbnail.short_description = 'photo'
+
+    list_display= ('id','thumbnail','first_name','last_name','designation','created_date',)
+
+    list_display_links = ('id','thumbnail','first_name',)
+
+    search_fields = ('first_name','last_name','designation',)
+
+    list_filter = ('designation',)
+    
+
+
+
+admin.site.register(Team,TeamAdmin)
+
