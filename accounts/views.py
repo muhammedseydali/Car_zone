ae5fafc99f8a9237ec288073948c44af24d59fb2
muhammed seydali <60098100+muhammedseydali@users.noreply.github.com>
2022-06-02 00:58:54 +0530

    accounts authentications completed

--- accounts/views.py
+++ accounts/views.py
@@ -14,7 +14,7 @@ def register(request):
         username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
-        confirmpassword = request.POST['confirmpassword']
+        confirmpassword = request.POST['confirm_password']
 
         if password == confirmpassword:
             if User.objects.filter(username=username).exists():
@@ -25,16 +25,16 @@ def register(request):
                     messages.error(request,'Email already exists')
                     return redirect('register')   
                 else:
-                    user = User.objects.create_user(firstname=firstname, lastname=lastname,username=username,email=email,password=password)
+                    user = User.objects.create_user(first_name=firstname, last_name=lastname,username=username,email=email,password=password)
                     auth.login(request, user)
                     messages.success(request, "you are logged in")
                     return redirect('dashboard')
                     user.save()     
                     messages.success(request,'You are registerd successfully')
                     return redirect('login')
-    else:
-        messages.error(request,'password does not match')
-        return redirect('register')    
+        else:
+            messages.error(request,'password does not match')
+            return redirect('register')    
 
     return render(request,'accounts/register.html')
 
@@ -44,20 +44,23 @@ def login(request):
         username = request.POST['username']
         password = request.POST['password']
         
-
         user = auth.authenticate(username=username,password=password)
 
         if user is not None:
             auth.login(request,user)
             messages.success(request, 'You are logged in')
-        return redirect('dashboard')
+            return redirect('dashboard')
 
-    else:
-        messages.error(request,'Invalid login credentials')
-        return redirect('login')
+        else:
+            messages.error(request,'Invalid login credentials')
+            return redirect('login')
     return render(request,'accounts/login.html')
 
 def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request,'You are successfully loggedout!!')
+        return redirect('home')
     return render(request,'accounts/logout.html')
 
 def dashboard(request):
